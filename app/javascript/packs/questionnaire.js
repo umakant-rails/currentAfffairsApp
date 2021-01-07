b45e050a2805563d2b39f02d9672329f78b67252
umakant-rails <rajputumakant@gmail.com>
2021-01-08 00:44:33 +0530

    add questionnaire form validation before submission

--- app/javascript/packs/questionnaire.js
+++ app/javascript/packs/questionnaire.js
@@ -12,21 +12,47 @@ var questionnaireFunctions = (function () {
     } else {
       $("#name").val("");
     }
+  },
+  setMessage = function(msg){
+    if(!$("#alert-message").hasClass("alert-danger")){
+      $("#alert-message").addClass("alert-danger");
+    }
+    $("#alert-message-span").html(msg);
+    $("#alert-message").show();
+    $("#alert-message").fadeOut(2000);
+  },
+  validateQuestionnaireForm = function(){
+    var isTrue = true;
+    var questionnaireType = $("#questionnaire_questionnaire_category_id").val();
+    var questionnaireName = $("#name").val().trim();
+
+    if(questionnaireType.length == 0){
+      isTrue = false;
+      questionnaireFunctions.setMessage("Questionnaire Type can't be blank.");
+    }else if(questionnaireName.length == 0){
+      isTrue = false;
+      questionnaireFunctions.setMessage("Questionnaire Name can't be blank.");
+
+    }
+    return isTrue;
   };
   return {
-    setCurrentAffairName: setCurrentAffairName
+    setCurrentAffairName: setCurrentAffairName,
+    validateQuestionnaireForm: validateQuestionnaireForm,
+    setMessage: setMessage
   }
 })();
 
 $(document).on("turbolinks:load", function(){
   $("#questionnaire_questionnaire_category_id").on("change", function(){
     questionnaireFunctions.setCurrentAffairName(this);
+    $("#questionnaire_submit").prop("disabled", false);
   });
-  /*$(“#questionnaire_form).validate({
-    debug: true,
-    rules: {
-    “questionnaire[questionnaire_category_id]“: {required: true},
-    “questionnaire[name]“: {required: true}
+  $("#name").on("focusout", function(){
+    var vl = $(this).val().trim();
+    console.log(vl);
+    if(vl.length > 0) {
+      $("#questionnaire_submit").prop("disabled", false);
     }
-  });*/
+  });
 });
