76b9d916719e70c49f51f80ee3dcf7ef23988f63
umakant-rails <rajputumakant@gmail.com>
2021-04-08 12:06:01 +0530

    remove current_affairs_controller

--- app/controllers/admin/current_affairs_controller.rb
+++
@@ -1,9 +0,0 @@
-class Admin::CurrentAffairsController < ApplicationController
-  before_action :authenticate_user!
-  layout 'admin'
-
-  def new
-    @questionnaire = Questionnaire.new
-  end
-
-end
