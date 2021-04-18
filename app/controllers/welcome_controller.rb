9f76cce1842f0d9c6f31802ea1517e6f310359b2
umakant-rails <rajputumakant@gmail.com>
2021-04-18 15:42:26 +0530

    html/css fixes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,9 @@
 class WelcomeController < ApplicationController
-  
+  layout 'application'  
+
   def index 
-    if current_user.blank?
-      @questionnaires = Questionnaire.all
-      @questionnaire = Questionnaire.joins(:questions).where(questionnaire_category: 1).order("questionnaires.created_at").last
-    else
-      redirect_to home_path
-    end
+    @questionnaires = Questionnaire.all
+    @questionnaire = Questionnaire.joins(:questions).where(questionnaire_category: 1).order("questionnaires.created_at").last
   end
 
 end
